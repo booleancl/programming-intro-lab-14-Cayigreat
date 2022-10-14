d594fe09ee40ca3f2111753a78f3a06a86c728dc
cayigreat <cayiarja@gmail.com>
2022-10-14 10:54:21 -0300

    cardinalidad de relación

--- migrations/4_seedData.sql
+++ migrations/4_seedData.sql
@@ -1,1 +1,5 @@
+\c blog
 
+\COPY users(email) FROM 'C:/Users/javil/intro_dev/programming-intro-lab-14-Cayigreat/data/users.csv' csv HEADER;
+\COPY posts FROM 'C:/Users/javil/intro_dev/programming-intro-lab-14-Cayigreat/data/posts.csv' csv HEADER;
+\COPY comments FROM 'C:/Users/javil/intro_dev/programming-intro-lab-14-Cayigreat/data/comentarios.csv' csv HEADER;
