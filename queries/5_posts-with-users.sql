6ea64acbd452ab0106118495048dd850370533af
cayigreat <cayiarja@gmail.com>
2022-10-19 09:14:49 -0300

    lazy y posts and comments

--- queries/5_posts-with-users.sql
+++ queries/5_posts-with-users.sql
@@ -1,1 +1,8 @@
 /* Seleccionar el correo del usuario, el id del post y el título de los posts */
+\c blog
+
+SELECT email, posts.id, title 
+FROM users 
+JOIN posts ON posts.user_id = users.id;
+
+
--- queries/6_comments-with-users.sql
+++ queries/6_comments-with-users.sql
@@ -1,7 +1,8 @@
+\c 
+
 /*
 Seleccionar el correo, id del usuario y el contenido de los comentarios
 */
 SELECT email, users.id, content
-FROM comments
-INNER JOIN users ON users.id = comments.user_id 
-WHERE users.email != 'usuario06@hotmail.com'
+FROM users
+JOIN comments ON users.id = comments.user_id 
